830fbeae60fe753a984c07a2672bb44cf1298bd6
ChirmansChallengeBBNA <ChirmansChallenge2018@gmail.com>
2018-06-19 08:51:04 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -1,96 +1,76 @@
-$(function () {
-    /* remove eloqua values*/
-     /* add this javascript before the end of your script tag to remove all the eloqua tags inside the textboxes */
-     var textboxes=document.querySelectorAll('input[type=text]');
-     if(textboxes){
-       for (var i = 0; i < textboxes.length; i++) {
-           textboxes[i].value=''; 
-       }
-   }
-        function getUrlQS(q, url) {
-            var vars = [],
-                hash;
-            if (typeof url == 'undefined') {
-                var url = window.location.search;
-            }
-            var hashes = url.slice(url.indexOf('?') + 1).split('&');
-            for (var i = 0; i < hashes.length; i++) {
-                hash = hashes[i].split('=');
-                vars.push(hash[0]);
-                vars[hash[0]] = hash[1];
-            }
-            var qs = (typeof vars[q] !== 'undefined') ? decodeURIComponent(vars[q]) : '';
-            return qs;
-        }
- 
-        $("form").each(function () {
-            var inputs = [];
-            var val = "";
-            $(this).find("input").each(function () {
-                if (this.attributes && this.attributes.name && this.attributes.name.value.toLowerCase() == "elqcampaignid") {
-                    inputs.push($(this));
-                    if (!val && $(this).val()) {
-                        val = $(this).val();
-                    }
-                }
-            });
- 
-            if (inputs.length > 0) {
-                while (inputs.length > 1) {
-                    inputs.pop().remove();
-                }
-                inputs[0].val(getUrlQS("elqCampaignId") || val);
-            }
-        });
-
-        function getQueryStringParamValue(keyvalue) {
-            var params = {};
-            var strURL = document.location.href;
-            var qs = '';
-            if (strURL.indexOf('?') != -1) {
-              qs = strURL.substr(strURL.indexOf('?') + 1)
-            }
-            if (qs.length == 0) {
-              return '';
-            }
-            qs = qs.replace(/\+/g, ' ');
-            var args = qs.split('&');
-            for (var i = 0; i < args.length; i++) {
-              var pair = args[i].split('=');
-              var name = decodeURIComponent(pair[0]);
-              var value = (pair.length == 2) ? decodeURIComponent(pair[1]) : name;
-              params[name] = value;
-            }
-            var qsparam = params[keyvalue];
-            return (qsparam != null) ? qsparam : '';
-          }
-          function setCampaignInfo(){
-            var form=document.querySelectorAll('.elq-form')[0];
-            if(form){
-                form.promocode.value =getQueryStringParamValue('promocode'); 
-                form.trkcode.value = getQueryStringParamValue('trackingcode');
-                form.utm_source.value = getQueryStringParamValue('utm_source');
-                form.utm_medium.value = getQueryStringParamValue('utm_medium');
-                form.elqCampaignId.value = getQueryStringParamValue('elqCampaignId');
-            }
-          }
-          setCampaignInfo();
-    });
-
- 
-
+var videoString = '//players.brightcove.net/68290866001/HJCdfoas_default/index.html?videoId='
 
 var app = new Vue({
     el: '#app',
     data: {
-        result: null
+        result: null,
+        sections: [
+            {name: 'Diversity & Inclusion', visible: true},
+            {name: 'Pick a Word', visible: false},
+            {name: 'Cultrural Months', visible: false}
+        ],
+        iframe: '',
+        videos:[
+            { vid: 'main', current: false, id: '2t6twcX' },
+            { vid: 'Ali Sartipzadeh', current: false, id: '2I1pFCj' },
+            { vid: 'Antonia Thomas', current: false, id: '2JP3mWv' },
+            { vid: 'Joe Hagedorn', current: false, id: '2M3VHjD' },
+            { vid: 'Keris Threatt', current: false, id: '2I1ztMC' },
+            { vid: 'Kristy Devich', current: false, id: '2JPhChW' },
+            { vid: 'Scottie Carter', current: false, id: '2MzFkwt' },
+            { vid: 'Tawny Elgatian', current: false, id: '2JLXLA0' },
+            { vid: 'Xing Gao', current: false, id: '2HXvbWN' },
+            { vid: 'Kareena Macasaet', current: false, id: '2JLYAc4' },
+            { vid: 'Rebecca Kern', current: false, id: '2HZkIdu' },
+            { vid: 'Rachel Martin', current: false, id: '2MDxS39' },
+            { vid: 'Madra Cochran', current: false, id: '2JMWSau' },
+            { vid: 'Molly Huie', current: false, id: '2t63eaC' },
+            { vid: 'Arielle Elliot', current: false, id: '2JMBl1R' },
+            { vid: 'Jerome Ashton', current: false, id: '2JUC7Wd' },
+            { vid: 'Teaganne Finn', current: false, id: '2t7Hmf3' },
+            { vid: 'Jazlyn Williams', current: false, id: '2MCyJRV' }
+        ],
+        modal:{
+            state: false,
+            videoNumber: 0
+        },
+        currentVideo: '<div class="loading"></div>'
     },
-    methods: {},
-    created(){
+    methods: {
+        test(i){
+            
+            var h = document.body.clientHeight;
+            
+            this.$el.scrollTo({top: (i * h), behavior: 'smooth'});
+            
+            console.log(document.body.clientHeight);
+            
+        },
+        showModal(x){
+            
+            this.modal.state = true;
+            //this.currentVideo = this.videos[x].content;
+            this.currentVideo = this.spliceFrame(this.videos[x].id);
+            
+        },
+        closeModal(){
+            
+            this.modal.state = false;
+            this.currentVideo = '<div class="loading"></div>';
+            
+        },
+        spliceFrame(link){
+            
+            var tag1 = '<iframe src="https://bcove.video/',
+                tag2 = '" allowfullscreen webkitallowfullscreen mozallowfullscreen style=\"position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; width: 100%; height: 100%;\"></iframe>'
+            
+            return tag1 + link + tag2;
+            
+        }
+    },
+    updated(){
         
-        //var x = location;
         
-        console.log(location.search);
         
     }
 })
